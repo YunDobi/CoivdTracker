a116c4a80f22f4b485059c3707bbacb7e8597338
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-08-08 15:33:23 -0700

    successfully fetching the data

--- covid-tracker/src/components/Header.vue
+++ covid-tracker/src/components/Header.vue
@@ -1,7 +1,8 @@
 <template>
   <header class="text-center bg-blue-800 text-white p-4 mb-10">
-    <div class="text-3xl md:text-5xl font-bold mb-3">Coivd-19 Tracker</div>
-    <p>API by <a class="text-blue-300" href="http://covid19api.com" target="_blank">http://covid19api.com</a></p>
+    <div class="text-3xl md:text-5xl font-bold mb-3"><i class="fa fa-virus-covid"></i> Coivd-19 Tracker</div>
+    <p>API by <a class="text-blue-300" href="http://covid19api.com" target="_blank">http://covid19api.com
+      </a></p>
   </header>
 </template>
 
@@ -10,5 +11,3 @@ export default {
   name: 'Header',
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
--- covid-tracker/src/views/HomeView.vue
+++ covid-tracker/src/views/HomeView.vue
@@ -1,14 +1,47 @@
 <template>
-  <div class="home">
-   Hello world
-  </div>
+  <main v-if="!loading">
+    Show data
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">
+      Fetching Data
+    </div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="">
+  </main>
 </template>
 
 <script>
 
 export default {
   name: 'HomeView',
-  components: {
-  }
+  components: {},
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      state: {},
+      countries: [],
+      loadingImage: require("../assets/logo.png")
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary');
+      const data = await res.json();
+      return data;
+    }
+  },
+  async created() {
+    const data = await this.fetchCovidData()
+  
+
+    this.dataDate = data.Date;
+    this.state = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+
+  },
 }
 </script>
