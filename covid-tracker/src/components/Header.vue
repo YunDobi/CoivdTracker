032aa299411c6f7fd3e7fa4dd18c763a5418dbfe
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-08-08 13:55:13 -0700

    header

---
+++ covid-tracker/src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb-3">Coivd-19 Tracker</div>
+    <p>API by <a class="text-blue-300" href="http://covid19api.com" target="_blank">http://covid19api.com</a></p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
